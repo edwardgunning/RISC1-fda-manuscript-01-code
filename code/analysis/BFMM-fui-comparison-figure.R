6c8c2ba58346e2b2dbaf8e996f9c8b314a056389
edwardgunning <edward.gunning@ul.ie>
2023-06-19 18:26:55 +0100

    add comparison to the FUI model.

---
+++ code/analysis/BFMM-fui-comparison-figure.R
@@ -0,0 +1,172 @@
+# -------------------------------------------------------------------------
+# Figures to compare point estimates and standard errors to 
+# fast univatiate inference methods by Cui (2022).
+# -------------------------------------------------------------------------
+
+# Packages: ---------------------------------------------------------------
+library(data.table) # CRAN v1.14.2
+library(tikzDevice) # CRAN v0.12.3.1
+library(ggpubr)     # CRAN v0.4.0
+library(ggplot2)    # CRAN v3.4.0
+
+
+# -------------------------------------------------------------------------
+source(here::here("code", "functions", "theme_gunning.R"))
+theme_gunning() # set theme
+# + some customisations for plots
+theme_update(panel.grid.major = element_blank(),
+             legend.key.size = unit(0.95,"line"))
+
+# rough guide for sizing of plot outputs:
+doc_width_cm <- 16
+doc_width_inches <- doc_width_cm *  0.3937
+
+# Path to save the outputs of analysis: -----------------------------------
+plots_path <- here::here("outputs", "figures")
+results_path <- here::here("outputs", "results")
+
+
+# -------------------------------------------------------------------------
+bootstrap_results <- readRDS(file.path(results_path, "bootstrap-results.rds"))
+fui_results_df <- readRDS(here::here("outputs", "results", "fui_comparison.rds"))
+fui_results_dt <- data.table(fui_results_df)
+
+
+# Format data for plotting: ----------------------------------------------
+
+
+parameter_results_dt <- bootstrap_results$parameter_results_dt
+stopifnot(nrow(parameter_results_dt) == nrow(fui_results_dt))
+
+combined_parameter_results_dt <- merge.data.table(x = parameter_results_dt,
+                                                  y = fui_results_dt,
+                                                  by = c("parameter", "dimension", "t"),
+                                                  all = TRUE)
+stopifnot(nrow(combined_parameter_results_dt) == nrow(parameter_results_dt))
+
+combined_parameter_results_dt[,
+                     beta_label_part_1 := fcase(
+                       parameter == "(Intercept)", "Intercept",
+                       parameter == "risinjured_greater_than_2_yr", "RIS: Injured $>2$ yr.",
+                       parameter == "risinjured_1_to_2_yr", "RIS: Injured $1-2$ yr.",
+                       parameter == "risinjured_less_than_1_yr", "RIS: Injured $<1$ yr.",
+                       parameter == "sexfemale", "Sex",
+                       parameter == "speed_cent", "Speed",
+                       parameter == "weight_kg_cent", "Weight",
+                       parameter == "height_cm_cent", "Height",
+                       parameter == "age_cent", "Age"
+                     )
+]
+combined_parameter_results_dt[, beta_label_part_1 := factor(
+  beta_label_part_1, 
+  levels = c(
+    "Intercept",
+    "Speed",
+    "Sex",
+    "RIS: Injured $>2$ yr.",
+    "RIS: Injured $1-2$ yr.",
+    "RIS: Injured $<1$ yr.",
+    "Age",
+    "Weight",
+    "Height"
+  ))]
+
+
+
+
+# -------------------------------------------------------------------------
+head(combined_parameter_results_dt)
+a1 <- ggplot(data = combined_parameter_results_dt[dimension == "hip"]) +
+  aes(x = t) +
+  facet_wrap(~ beta_label_part_1, scales = "free_y") +
+  geom_line(aes(y = point_est, colour = "Current", linetype = "Point Estimate")) +
+  geom_line(aes(y = fui_point_est, colour = "FUI", linetype = "Point Estimate")) +
+  geom_line(aes(y = pw_fui_lower_analytic, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_fui_upper_analytic, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_wald_lower, colour = "Current", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_wald_upper, colour = "Current", linetype = "Pointwise CI")) +
+  scale_colour_manual(values = c("red4", "blue4")) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_linetype_manual(values = c(1, 2)) +
+  labs(x = "Normalised Time ($\\%$ of Stride)",
+       y = "Coefficient Function $\\beta^{(hip)}_a (t)$",
+       title = "Hip") +
+  guides(linetype = guide_legend(override.aes = list(linewidth = 1)),
+         colour = guide_legend(override.aes = list(linewidth = 1)))
+
+a2 <- ggplot(data = combined_parameter_results_dt[dimension == "knee"]) +
+  aes(x = t) +
+  facet_wrap(~ beta_label_part_1, scales = "free_y") +
+  geom_line(aes(y = point_est, colour = "Current", linetype = "Point Estimate")) +
+  geom_line(aes(y = fui_point_est, colour = "FUI", linetype = "Point Estimate")) +
+  geom_line(aes(y = pw_fui_lower_analytic, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_fui_upper_analytic, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_wald_lower, colour = "Current", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_wald_upper, colour = "Current", linetype = "Pointwise CI")) +
+  scale_colour_manual(values = c("red4", "blue4")) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_linetype_manual(values = c(1, 2)) +
+  labs(x = "Normalised Time ($\\%$ of Stride)",
+       y = "Coefficient Function $\\beta^{(hip)}_a (t)$",
+       title = "Knee") +
+  guides(linetype = guide_legend(override.aes = list(linewidth = 1)),
+         colour = guide_legend(override.aes = list(linewidth = 1)))
+
+
+tikz(file.path(plots_path, "fui-comparison-analytic.tex"),
+     width = 1 * doc_width_inches, 
+     height = 1.5 * (doc_width_inches),
+     standAlone = TRUE)
+ggarrange(a1, a2, common.legend = TRUE, nrow = 2, legend = "bottom")
+dev.off()
+
+tinytex::lualatex(file.path(plots_path, "fui-comparison-analytic.tex"))
+
+
+b1 <- ggplot(data = combined_parameter_results_dt[dimension == "hip"]) +
+  aes(x = t) +
+  facet_wrap(~ beta_label_part_1, scales = "free_y") +
+  geom_line(aes(y = point_est, colour = "Current", linetype = "Point Estimate")) +
+  geom_line(aes(y = fui_point_est, colour = "FUI", linetype = "Point Estimate")) +
+  geom_line(aes(y = pw_fui_lower_boot, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_fui_upper_boot, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_boot_lower, colour = "Current", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_boot_upper, colour = "Current", linetype = "Pointwise CI")) +
+  scale_colour_manual(values = c("red4", "blue4")) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_linetype_manual(values = c(1, 2)) +
+  labs(x = "Normalised Time ($\\%$ of Stride)",
+       y = "Coefficient Function $\\beta^{(hip)}_a (t)$",
+       title = "Hip") +
+  guides(linetype = guide_legend(override.aes = list(linewidth = 1)),
+         colour = guide_legend(override.aes = list(linewidth = 1)))
+
+b2 <- ggplot(data = combined_parameter_results_dt[dimension == "knee"]) +
+  aes(x = t) +
+  facet_wrap(~ beta_label_part_1, scales = "free_y") +
+  geom_line(aes(y = point_est, colour = "Current", linetype = "Point Estimate")) +
+  geom_line(aes(y = fui_point_est, colour = "FUI", linetype = "Point Estimate")) +
+  geom_line(aes(y = pw_fui_lower_boot, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_fui_upper_boot, colour = "FUI", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_boot_lower, colour = "Current", linetype = "Pointwise CI")) +
+  geom_line(aes(y = pw_boot_upper, colour = "Current", linetype = "Pointwise CI")) +
+  scale_colour_manual(values = c("red4", "blue4")) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  scale_linetype_manual(values = c(1, 2)) +
+  labs(x = "Normalised Time ($\\%$ of Stride)",
+       y = "Coefficient Function $\\beta^{(hip)}_a (t)$",
+       title = "Knee") +
+  guides(linetype = guide_legend(override.aes = list(linewidth = 1)),
+         colour = guide_legend(override.aes = list(linewidth = 1)))
+
+tikz(file.path(plots_path, "fui-comparison-bootstrap.tex"),
+     width = 1 * doc_width_inches, 
+     height = 1.5 * (doc_width_inches),
+     standAlone = TRUE)
+ggarrange(b1, b2, common.legend = TRUE, nrow = 2, legend = "bottom")
+dev.off()
+
+tinytex::lualatex(file.path(plots_path, "fui-comparison-bootstrap.tex"))
+
+
+
---
+++ code/analysis/BFMM-fui-comparison.R
@@ -0,0 +1,128 @@
+# -------------------------------------------------------------------------
+# Compare point estimates and standard errors to 
+# fast univatiate inference methods by Cui (2022).
+# note I have modified the bootstrap approach in lfosr3s to  use
+# pseudo IDs in cluster labels.
+# -------------------------------------------------------------------------
+
+# Packages ----------------------------------------------------------------
+library(data.table) # CRAN v1.14.0
+library(ggplot2)    # CRAN v3.3.5
+library(fda)        # CRAN v5.5.1
+library(multifamm)  # CRAN v0.1.1
+library(sparseFLMM) # CRAN v0.4.1
+source("code/functions/FUI-functions/lfosr3s.R")
+source("code/functions/FUI-functions/lfosr3s-updated.R")
+
+source(file = here::here(
+  "code",
+  "functions",
+  "functions-helper-smoothing.R"
+))
+source(here::here(
+  "code/functions/rough_fit_mfamm_model.R"
+))
+
+data_path <- here::here("data")
+subject_side_coef_reg <- readRDS(file.path(data_path, "subject_side_coef_reg.rds"))
+
+# set up basis to represent the data.
+bspl80 <- fda::create.bspline.basis(
+  rangeval = c(0, 100),
+  nbasis = 80,
+  norder = 4)
+
+
+# Prepare data for FUI: ---------------------------------------------------
+subject_side_eval_reg <- copy(subject_side_coef_reg)
+subject_side_eval_reg[, paste0(0:100) := {
+  coef_mat <- coef_to_mat(.SD)
+  fd_object <- fd(coef = coef_mat, basisobj = bspl80)
+  fd_eval <- eval.fd(evalarg = 0:100, fdobj = fd_object)
+  return_list <- get_list_of_rows(fd_eval)
+  names(return_list) <- 0:100
+  return_list},
+  .SDcols = paste0("lm_coef_", 1:80)]
+subject_side_eval_reg[, paste0("lm_coef_", 1:80) := NULL]
+subject_side_eval_reg <- subject_side_eval_reg[location %in% c("Hip", "Knee")]
+covariates_df <- data.frame(subject_side_eval_reg[, .(location = location, ris = factor(retrospective_injury_status, ordered = FALSE), sex, speed_cent, weight_kg_cent, height_cm_cent, age_cent, subject_id)])
+covariates_df$Y <- as.matrix(subject_side_eval_reg[, paste0(0:100)])
+
+fui_hip_analytic_time <- system.time(
+  fui_hip_analytic <- lfosr3s(formula = Y ~ ris + sex + speed_cent +  weight_kg_cent + height_cm_cent + age_cent + (1|subject_id), 
+                     data = covariates_df[covariates_df$location == "Hip",], 
+                     family = "gaussian",
+                     var = TRUE,
+                     analytic = TRUE)
+)["elapsed"]
+
+
+fui_knee_analytic_time <- system.time(
+  fui_knee_analytic <- lfosr3s(formula = Y ~ ris + sex + speed_cent +  weight_kg_cent + height_cm_cent + age_cent + (1|subject_id), 
+                   data = covariates_df[covariates_df$location == "Knee",], 
+                   family = "gaussian",
+                   var = TRUE,
+                   analytic = TRUE))["elapsed"]
+
+
+fui_hip_boot_time <- system.time(
+  fui_hip_boot <- lfosr3s_updated(formula = Y ~ ris + sex + speed_cent +  weight_kg_cent + height_cm_cent + age_cent + (1|subject_id),
+                     data = covariates_df[covariates_df$location == "Hip",],
+                     family = "gaussian",
+                     var = TRUE,
+                     analytic = FALSE,
+                     parallel = TRUE))["elapsed"]
+
+
+fui_knee_boot_time <- system.time(
+  fui_knee_boot <- lfosr3s_updated(formula = Y ~ ris + sex + speed_cent +  weight_kg_cent + height_cm_cent + age_cent + (1|subject_id),
+                             data = covariates_df[covariates_df$location == "Knee",],
+                             family = "gaussian",
+                             var = TRUE,
+                             analytic = FALSE,
+                             parallel = TRUE))["elapsed"]
+
+
+
+t <- 0:100
+fui_results_df <- data.frame()
+for(i in 1:9) {
+  param <- rownames(fui_hip_analytic$betaHat)[i]
+  fui_point_est <- fui_hip_analytic$betaHat[i,]
+  
+  se_boot <- sqrt(diag((1/1.2) * fui_hip_boot$betaHat.var[,,i]))
+  se_analytic <- sqrt(diag((1/1) * fui_hip_analytic$betaHat.var[,,i]))
+  
+  df_i <- data.frame(t = t,
+             dimension = "hip",
+             parameter = param,
+             fui_point_est = fui_point_est,
+             pw_fui_lower_boot = fui_point_est - 2 * se_boot,
+             pw_fui_upper_boot = fui_point_est + 2 * se_boot,
+             pw_fui_lower_analytic = fui_point_est - 2 * se_analytic,
+             pw_fui_upper_analytic = fui_point_est + 2 * se_analytic)
+  fui_results_df <- rbind(fui_results_df, df_i)
+}
+for(i in 1:9) {
+  param <- rownames(fui_knee_analytic$betaHat)[i]
+  fui_point_est <- fui_knee_analytic$betaHat[i,]
+  
+  se_boot <- sqrt(diag((1/1.2) * fui_knee_boot$betaHat.var[,,i]))
+  se_analytic <- sqrt(diag((1/1) * fui_knee_analytic$betaHat.var[,,i]))
+  
+  df_i <- data.frame(t = t,
+                     dimension = "knee",
+                     parameter = param,
+                     fui_point_est = fui_point_est,
+                     pw_fui_lower_boot = fui_point_est - 2 * se_boot,
+                     pw_fui_upper_boot = fui_point_est + 2 * se_boot,
+                     pw_fui_lower_analytic = fui_point_est - 2 * se_analytic,
+                     pw_fui_upper_analytic = fui_point_est + 2 * se_analytic)
+  fui_results_df <- rbind(fui_results_df, df_i)
+}
+
+
+
+
+saveRDS(object = fui_results_df, file = here::here("outputs", "results", "fui_comparison.rds"))
+
